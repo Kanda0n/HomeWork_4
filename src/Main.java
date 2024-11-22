8443ded8fa0b87b30602dd0421978222148daf87
Kanda0n <kononchenko1996@gmail.com>
2024-11-22 12:28:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Домашняя работа по теме Условные операторы1");
+
+        System.out.println("Task 1");
+
+
+    }
+}
